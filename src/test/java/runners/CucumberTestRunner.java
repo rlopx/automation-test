3e3798ad77afc8bee978407a637f63db85d50d63
rlopx <lopes.rosa.90@gmail.com>
2025-04-15 10:40:15 +0100

    Parallel test execution

--- src/test/java/runners/CucumberTestRunner.java
+++ src/test/java/runners/CucumberTestRunner.java
@@ -2,10 +2,22 @@ package runners;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
 @CucumberOptions(features = {"classpath:features"}, glue = {"stepDefinitions"},
         monochrome = true, dryRun = false,
         plugin = {"pretty", "html:target/cucumber"})
-// monochrome makes terminal logs more readable; dryRun true = identifies undefined steps; plugin pretty prints tests steps, HTML creates a html report
+/*
+ * tags selects which tests to run (must have @word in feature file) [ex: tags = "@login" ]
+ * monochrome makes terminal logs more readable;
+ * dryRun true = identifies undefined steps;
+ * plugin pretty prints tests steps, HTML creates an html report
+ */
+
 public class CucumberTestRunner extends AbstractTestNGCucumberTests {
+    @Override
+    @DataProvider(parallel = false) // allows parallel testing
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
