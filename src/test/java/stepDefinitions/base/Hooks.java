d4b5d44b1bbabbb24a4ea5c81fdb83624724794d
rlopx <lopes.rosa.90@gmail.com>
2025-04-15 10:52:21 +0100

    Attach images to failing steps

--- src/test/java/stepDefinitions/base/Hooks.java
+++ src/test/java/stepDefinitions/base/Hooks.java
@@ -1,8 +1,14 @@
 package stepDefinitions.base;
 
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+
+import java.sql.Timestamp;
 
 import static driver.DriverFactory.cleanupDriver;
 import static driver.DriverFactory.getDriver;
@@ -13,6 +19,18 @@ public class Hooks {
     @Before
     public void setup() {
         getDriver();
+    }
+
+    @AfterStep
+    public void captureExceptionImage(Scenario scenario) {
+        if (scenario.isFailed()) {
+            Timestamp timestamp = new Timestamp(System.currentTimeMillis()); //long type variable
+            String timeMilliseconds = Long.toString(timestamp.getTime()); //convert long to string
+
+            byte[] screenshot = ((TakesScreenshot) getDriver())
+                    .getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", timeMilliseconds); //attaches image to failed steps
+        }
     }
 
     @After
